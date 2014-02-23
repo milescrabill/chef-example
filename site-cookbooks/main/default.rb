03e5e4c14bc60c988b0ec220ffec24752c72e9eb
Miles Crabill <miles@milescrabill.com>
2014-02-23 12:51:29 -0800

    Scapy working w/o virtualenv

--- site-cookbooks/main/default.rb
+++
@@ -1,4 +0,0 @@
-package "scapy" do
-	provider Chef::Provider::PythonPip
-	action :install
-end
--- site-cookbooks/main/recipes/default.rb
+++ site-cookbooks/main/recipes/default.rb
@@ -1,3 +1,16 @@
-pip_install "scapy" do
-	action :install
-end
+package "python-crypto"
+package "python-pyx"
+package "tcpdump"
+
+# python_virtualenv "/home/ubuntu/ve" do
+# 	interpreter "python2.5"
+# 	owner "ubuntu"
+# 	group "ubuntu"
+# 	action :create
+# end
+
+python_pip "scapy" do
+ 	action :install
+ 	version "2.2.0-dev"
+ 	# virtualenv "/home/ubuntu/ve"
+ end
